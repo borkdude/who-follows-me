15d684b75a64a95745f003af6fdd954b8edca247
Michiel Borkent <michielborkent@gmail.com>
2014-08-24 22:50:19 +0200

    Updated twitter-api dependency

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
    :dependencies [[org.clojure/clojure "1.5.1"]
                   [compojure "1.1.3"]
                   [ring/ring-jetty-adapter "1.1.6"]
-                  [twitter-api "0.7.5"]
+                  [twitter-api "0.7.6"]
                   ;; pulls in clj-oauth
                   [enlive "1.0.1"]]
                   ;;[me.raynes/laser "0.1.11"]
